54521fcd76a6696d3357d5071509fd095a8c6f12
varnaraj <64960978+varanaraj@users.noreply.github.com>
2021-07-28 10:14:57 +0530

    Update 2.c

--- lab03_CO321/2.c
+++ lab03_CO321/2.c
@@ -1,11 +1,10 @@
+
 /*
-Q2
+Q3
 XTAL = 16MHz → Txtal _clock = 1/16 μs
 prescaler 1:1024
 Tcounter_clock = 1024 × 1/16 μs = 64μs
 so max interval =256 x 64=16384 μs =16.384ms
-So we can't increase the delay to 500ms
-*/
-/*
+
 highest countable time interval = 16.384ms
 */
