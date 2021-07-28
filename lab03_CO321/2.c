1a69ee031c9a10c3eeb1e159f91653a42fa8d717
Thanujan <58055657+thanujan96@users.noreply.github.com>
2021-07-28 10:42:01 +0530

    Update 2.c

--- lab03_CO321/2.c
+++ lab03_CO321/2.c
@@ -1,11 +1,15 @@
 /*
 Q2
 XTAL = 16MHz → Txtal _clock = 1/16 μs
-prescaler 1:1024
-Tcounter_clock = 1024 × 1/16 μs = 64μs
-so max interval =256 x 64=16384 μs =16.384ms
+with  timer0 we only can use upto 255 counts, so we need less number clock counts for maximum time
+there fore we are taking maximum pre scale 
+maximum prescale 1:1024
+counter clock of timer0 with prescale = 1024 × 1/16 μs = 64μs
+so max interval that we can take from timer0 =256 x 64=16384 μs =16.384ms
 So we can't increase the delay to 500ms
 */
 /*
 highest countable time interval = 16.384ms
 */
+
+
